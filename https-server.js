3d5d9b474f347f3302911932c66bffd2b22d04fc
Jason Sturm <jsturm@kiosk.com>
2018-03-24 17:09:53 -0600

    adding http as well as https server files

--- https-server.js
+++ https-server.js
@@ -1,31 +1,45 @@
+// determines apps base director
+// https://www.abeautifulsite.net/determining-your-apps-base-directory-in-nodejs
 global.__basedir = __dirname;
+
+// TLS variables
+var fs = require('fs');
+var https = require('https');
+var options = {
+    key: fs.readFileSync('sslcert/server.key', 'utf8'),
+    cert: fs.readFileSync('sslcert/server.crt', 'utf8')
+};
+
 const express = require('express');
 const server = express();
+var httpsServer = https.createServer(options, server);
+
+// Node.js body parsing middleware
 var bodyParser = require('body-parser');
+
 var gpioUtil = require(`${__basedir}/services/gpio.service`);
-var fs = require('fs');
 var ip = require(`${__basedir}/services/ip.service`);
 var logger = require(`${__basedir}/logger`);
 
+// initialize logger
 require('winston-logs-display')(server, logger);
 
+// route handler variables
 var defaultRoute = require(`${__basedir}/route_methods/index`);
-var water = require(`${__basedir}/route_methods/water`);
 var powerStrip = require(`${__basedir}/route_methods/powerStrip`);
 
-server.use((req, res, next) => {
+httpsServer.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-server.use(bodyParser.json());
+httpsServer.use(bodyParser.json());
 
-server.use('/', defaultRoute);
-// server.use('/water', water);
-server.use('/sockets', powerStrip);
+httpsServer.use('/', defaultRoute);
+httpsServer.use('/sockets', powerStrip);
 
-server.listen(5555, () => {
+httpsServer.listen(5555, () => {
     // set up rpio and gpio pins
     var allOutputPins = [3, 5, 7, 11, 13, 15, 19, 21];
     gpioUtil.initOutPins(allOutputPins);
@@ -33,4 +47,4 @@ server.listen(5555, () => {
     logger.info(`Server running at http://${ip.getIP()}:5555/`);
 });
 
-module.exports = server;
+module.exports = httpsServer;
